f6476334f4ef59b2405c1badf244a78fb8530ad8
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-11-14 14:43:09 +0200

    sqlite added, index empty

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,37 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {open} from 'sqlite';
+import sqlite3 from 'sqlite3';
 
 type Data = {
-  name: string
-}
+    name: string;
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    const query = req.query;
+
+    const db = await open({
+        filename: 'database.db',
+        driver: sqlite3.Database,
+    });
+
+    // await db.exec('DROP TABLE IF EXISTS testi');
+
+    await db.exec(`
+    CREATE TABLE IF NOT EXISTS testi (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        t VARCHAR(10)
+    )
+    `);
+    await db.run(
+        'INSERT INTO testi (t) VALUES (?)',
+        `${JSON.stringify(query)}`
+    );
+
+    const result = await db.all('SELECT * FROM testi');
+
+    console.log(result);
+    res.status(200).json({name: 'John Doe'});
 }
