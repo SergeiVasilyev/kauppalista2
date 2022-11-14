11b4c2b62a4dbfadd8d3eeffebee18decda7bc0b
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-11-14 11:06:31 +0200

    Links added

--- pages/joku/[juttuId]/[toinenId].tsx
+++ pages/joku/[juttuId]/[toinenId].tsx
@@ -1,13 +1,18 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
+
 
 export default function Post() {
     const router = useRouter()
     const { juttuId, toinenId } = router.query
 
+    console.log(router.query)
+
     return (
     <div>
         <h1>Tämä on joku sivu</h1>
         <p>SIVU ID { juttuId } { toinenId }</p>
+        <Link href="/joku/sivu/"></Link>
     </div>
     )
     
--- pages/joku/sivu.tsx
+++ pages/joku/sivu.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
+
+
 export default function Page() {
     return (
     <div>
         <h1>Tämä on joku sivu</h1>
         <p>SIVU 1</p>
+
+        <Link href="/joku/juttu3/toinen4">Juttu kolmonen kohdasta 4</Link>
     </div>
     )
 }
