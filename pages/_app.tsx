9c9135ccd58c4dd23451f7482cef70a9344cad93
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-11-14 13:33:59 +0200

    app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,17 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 // import Layout from '../components/layout'
 import Layout from '@/components/layout'
+import Head from 'next/head'
 
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout><Component {...pageProps} /></Layout> 
+  return (
+    <Layout>
+      <Head>
+        <title>Kauppalista</title>
+        <meta />
+      </Head>
+      <Component {...pageProps} />
+    </Layout> 
+  )
 }
