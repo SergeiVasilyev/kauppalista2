a3c4265fe499760ea90b615370216e337d43155d
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2022-11-28 12:55:34 +0200

    listat updated

--- pages/listat.tsx
+++ pages/listat.tsx
@@ -1,33 +1,52 @@
-import Link from 'next/link'
-import { Page } from '@/components/Page'
+import Link from 'next/link';
 
+import {Page} from '@/components/Page';
+import {GetServerSideProps} from 'next';
+import {ShopList} from '@/utils/types';
+import {propTypes} from 'react-bootstrap/esm/Image';
+
+type PropsType = {
+    listat: ShopList[];
+};
 
 const LIST_ITEMS = [
-    {id: 1, text: "Eka kauppalista"},
-    {id: 2, text: "Toka kauppalista"},
-    {id: 3, text: "kolmas kauppalista"},
-]
+    {id: 1, text: 'Eka kauppalista'},
+    {id: 2, text: 'Toka kauppalista'},
+    {id: 3, text: 'Kolmas kauppalista'},
+];
 
-export default function ListatSivu() {
-    function ListItem({id, text}: {id: number, text: string}) {
+export default function ListatSivu({listat}: PropsType) {
+    function ListaListItem({id, name}: {id: number; name: string}) {
         return (
-        <li>
-            <Link href="listat/{id}">
-                ({id}){text}
-            </Link>
-        </li>
-        )
+            <li>
+                <Link href={`/lista/${id}`}>
+                    ({id}) {name}
+                </Link>
+            </li>
+        );
     }
+
     return (
         <Page title="Listat">
             <ul>
-            {LIST_ITEMS.map((item) => (
-                <ListItem id={item.id} text={item.text} />
-            )
-            )}
+                {listat.map((lista) => (
+                    <ListaListItem id={lista.id} name={lista.name} />
+                ))}
             </ul>
         </Page>
     );
 }
 
+export const getServerSideProps: GetServerSideProps<PropsType> = async (
+    context
+) => {
+    const host: string = context.req.headers.host ?? 'localhost';
+    const proto = /^localhost:?/.test(host) ? 'http' : 'https';
+    const response = await fetch(`${proto}://${host}/api/listat`);
+    const listat: ShopList[] = await response.json();
+
+    return {
+        props: {listat},
+    };
+};
 
